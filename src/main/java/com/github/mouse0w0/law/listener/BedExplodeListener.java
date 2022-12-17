4dc04c1683fcbf90df53983c0627b287ddd2bc3b
Mouse <mouse0w0@qq.com>
2022-12-18 00:03:14 +0800

    Fix prevent-bed-explosion and prevent-respawn-anchor-explosion

--- src/main/java/com/github/mouse0w0/law/listener/BedExplodeListener.java
+++ src/main/java/com/github/mouse0w0/law/listener/BedExplodeListener.java
@@ -3,11 +3,13 @@ package com.github.mouse0w0.law.listener;
 import com.github.mouse0w0.law.config.Law;
 import com.github.mouse0w0.law.util.EnumUtils;
 import org.bukkit.Material;
+import org.bukkit.World.Environment;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 import java.util.Set;
 
@@ -17,10 +19,16 @@ public class BedExplodeListener implements Listener {
             "LIGHT_BLUE_BED", "YELLOW_BED", "LIME_BED", "PINK_BED", "GRAY_BED",
             "LIGHT_GRAY_BED", "CYAN_BED", "PURPLE_BED", "BLUE_BED", "BROWN_BED",
             "GREEN_BED", "RED_BED", "BLACK_BED");
-    
+
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onBlockExplode(BlockExplodeEvent e) {
-        Block block = e.getBlock();
+    public void onBlockExplode(PlayerInteractEvent e) {
+        if (e.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+        Block block = e.getClickedBlock();
+        if (block.getWorld().getEnvironment() == Environment.NORMAL) {
+            return;
+        }
         if (BED.contains(block.getType())) {
             if (Law.get(block.getLocation()).preventBedExplosion) {
                 e.setCancelled(true);
--- src/main/java/com/github/mouse0w0/law/listener/RespawnAnchorExplodeListener.java
+++ src/main/java/com/github/mouse0w0/law/listener/RespawnAnchorExplodeListener.java
@@ -2,19 +2,37 @@ package com.github.mouse0w0.law.listener;
 
 import com.github.mouse0w0.law.config.Law;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.type.RespawnAnchor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class RespawnAnchorExplodeListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onBlockExplode(BlockExplodeEvent e) {
-        Block block = e.getBlock();
+    public void onBlockExplode(PlayerInteractEvent e) {
+        if (e.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
+        Block block = e.getClickedBlock();
+        if (block.getWorld().getEnvironment() == World.Environment.NETHER) {
+            return;
+        }
         if (block.getType() == Material.RESPAWN_ANCHOR) {
             if (Law.get(block.getLocation()).preventRespawnAnchorExplosion) {
-                e.setCancelled(true);
+                RespawnAnchor respawnAnchor = (RespawnAnchor) block.getBlockData();
+                int charges = respawnAnchor.getCharges();
+                if (charges == 0) {
+                    return;
+                }
+                ItemStack item = e.getItem();
+                if (item == null || item.getType() != Material.GLOWSTONE || charges == respawnAnchor.getMaximumCharges()) {
+                    e.setCancelled(true);
+                }
             }
         }
     }
